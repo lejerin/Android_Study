98fa3c9f834ead1b8cbfa8edd9d18b723f01d8c2
lejerin <lejerin1112@gmail.com>
2021-01-03 10:33:37 +0900

    chart custom 비율 변경

--- android-kotlin-drawing-custom-views-master/app/src/main/java/com/example/android/customfancontroller/MainActivity.kt
+++ android-kotlin-drawing-custom-views-master/app/src/main/java/com/example/android/customfancontroller/MainActivity.kt
@@ -18,11 +18,17 @@ package com.example.android.customfancontroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
+
+
+
 }
